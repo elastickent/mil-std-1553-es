64fcb7f9e18ba1df1e0233dbae4bc0a3bd6aecc6
elastickent <kent@elastic.co>
2019-07-10 10:08:31 -0400

    Update docker_build_run.sh

--- docker_build_run.sh
+++ docker_build_run.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/bash
 
 docker build . -t ch10-1553-es
-docker run -it ch10-1553-es:latest  /bin/bash
+docker run -v `pwd`/data_sets/:/data_sets -i --log-driver=none -a stdin -a stdout -a stderr ch10-1553-es:latest idmp1553 -i /data_sets/TTC-1553_107_132248.ch10 2>/dev/null|grep "Ch" | filebeat -c ./filebeat.yml -e
